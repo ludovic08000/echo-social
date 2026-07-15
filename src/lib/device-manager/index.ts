9ee15e8f6d6c0a45bdc9f3814734be3b0e47d92b
ludovic08000 <37050827+ludovic08000@users.noreply.github.com>
2026-07-15 19:25:45 +0200

    refactor(e2ee): expose unified device manager API

---
+++ src/lib/device-manager/index.ts
@@ -0,0 +1,7 @@
+export * from './currentDevice';
+export * from './health';
+export * from './lifecycle';
+export * from './operationLock';
+export * from './prekeyRepair';
+export * from './resync';
+export * from './sessionGate';
